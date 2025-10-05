e8448cdc6adc49d11d413668550aad00244eb0b5
Surendra Kumar <surendra.aratikatla@knacksystems.com>
2025-10-05 16:15:26 +0530

    added comments

--- ProjectTST/CustomActions/test.py
+++ ProjectTST/CustomActions/test.py
@@ -3,5 +3,6 @@ def deleteQuoteMsgs():
     for msg in quoteInfo.Messages:
         if str(msg.Content) == "Items added successfully!" or str(msg.Content) == "Items not found for the provided reference quote number." or str(msg.Content) == "Invalid reference quote number, please check.":
             quoteInfo.DeleteMessage(msg.Id)
+            # added comments
 
 deleteQuoteMsgs()
